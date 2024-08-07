48ced418b077b1eda7cfc4c3d283ebc7709eaeb7
jlh630 <2379746504@qq.com>
2024-08-08 00:15:14 +0800

    0808

---
+++ 94.binary-tree-inorder-traversal.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ * @lcpr version=30204
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	var dfs func(*TreeNode)
+	res := make([]int, 0)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		dfs(root.Left)
+		res = append(res, root.Val)
+		dfs(root.Right)
+	}
+	dfs(root)
+	return res
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+*/
+
