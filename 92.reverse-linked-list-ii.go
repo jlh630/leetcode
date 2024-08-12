3d11884968abd01394b649138cdb1dad9f8010ea
jlh630 <2379746504@qq.com>
2024-08-12 13:40:22 +0800

    0812

---
+++ 92.reverse-linked-list-ii.go
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=92 lang=golang
+ * @lcpr version=30204
+ *
+ * [92] 反转链表 II
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	thead := &ListNode{Val: -1}
+	thead.Next = head
+	pre := thead
+
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+	node := pre.Next
+	for i := 0; i < right-left; i++ {
+		next := node.Next
+		node.Next = next.Next
+		next.Next = pre.Next
+		pre.Next = next
+	}
+	return thead.Next
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n2\n4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5]\n1\n1\n
+// @lcpr case=end
+
+*/
+
