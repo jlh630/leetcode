d3432ee802e5e1221704e087ba21d0c6f807fbf1
jlh630 <2379746504@qq.com>
2024-08-19 17:16:14 +0800

    0819

---
+++ 63.unique-paths-ii.go
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=golang
+ * @lcpr version=30204
+ *
+ * [63] 不同路径 II
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+
+	n := len(obstacleGrid)
+	m := len(obstacleGrid[0])
+	if obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1 {
+		return 0
+	}
+	dp := make([]int, m)
+	dp[0] = 1
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+
+			if obstacleGrid[i][j] == 1 {
+				dp[j] = 0
+				continue
+			}
+			if j != 0 && obstacleGrid[i][j-1] == 0 {
+				dp[j] += dp[j-1]
+			}
+		}
+	}
+
+	return dp[m-1]
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [[0,0,0],[0,1,0],[0,0,0]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[0,1],[0,0]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[0,0],[0,1]]\n
+// @lcpr case=end
+
+*/
+
